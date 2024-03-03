3db3512312abd04f66b1dd3c374c3a01146512cb
Your Name <you@example.com>
2024-03-03 23:19:22 +0800

    merge

--- utils_prune.js
+++ utils_prune.js
@@ -12,15 +12,9 @@ const math_ = require('./util/math.js');
 const conf = require('./util/conf.js');
 const log = require('./util/log.js');
 const winston = require('winston');
-<<<<<<< HEAD
-const plattool = require('./util/plattool.js');
-const sysarg = require('./util/sysarg.js');
-
-=======
 const plattool = require('./util/plattool');
 const sysarg = require('./util/sysarg');
 const env = require('./util/env.js');
->>>>>>> 7277f84d66832d12cb6601508e31e28ae87fed3f
 module.exports = {
     toString: () => '[class UtilPrune]',
     date,
@@ -37,10 +31,6 @@ module.exports = {
     conf:conf,
     plattool,
     log,
-<<<<<<< HEAD
-    sysarg
-=======
     sysarg,
     env
->>>>>>> 7277f84d66832d12cb6601508e31e28ae87fed3f
 };
